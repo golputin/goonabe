2e047b1e623da75662920edbabe8665c15fd06ab
golputin <golputinsvy@gmail.com>
2026-08-04 19:10:42 +0700

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -4,4 +4,4 @@ export const PRE_LAUNCH = true;
 
 // Token details — fill TOKEN_CA after the token launches.
 export const TOKEN_TICKER = 'KLANKO';
-export const TOKEN_CA = '0x';
+export const TOKEN_CA = '0x6d3bb04ce67b87015a802003fa815911cadb008b';
